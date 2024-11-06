322ac1df40429d4be5d4176170c78f7022fd634b
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-11-06 17:36:40 +0100

    Changed name

--- components/ui/badge/BadgeCard.tsx
+++ components/ui/badge/BadgeCard.tsx
@@ -1,15 +1,15 @@
 import { motion } from "framer-motion";
-import { Button } from "./button";
 import Link from "next/link";
+import BadgeClass from "@/lib/classes/BadgeClass";
+import { Button } from "../button";
 
 interface BadgeProps {
-  index: number;
-  image: string;
-  title: string;
-  unlocked?: boolean;
+  badge: BadgeClass;
 }
 
-export default function Badge({ image, title, index, unlocked = true }: BadgeProps) {
+export default function BadgeCard({ badge }: BadgeProps) {
+  const { image, title, index, unlocked } = badge.getBadgeInfo();
+
   return (
     <motion.div
       className="flex flex-col justify-start items-center w-full p-3 gap-2 bg-secondary rounded-lg"
@@ -20,7 +20,7 @@ export default function Badge({ image, title, index, unlocked = true }: BadgePro
       <h1 className="text-sm text-nowrap font-black">{title}</h1>
       <img alt="badge image" src={image}></img>
       <Button className="w-full h-6">
-        <Link href={`/user/badges/${index}`}>Details</Link>
+        <Link href={`/user/badge/${index}`}>Details</Link>
       </Button>
     </motion.div>
   );
