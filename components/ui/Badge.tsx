c2ee1245710b2f2573275d1c2650933058666593
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-11-06 15:48:17 +0100

    Created Badge component

---
+++ components/ui/Badge.tsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import { Button } from "./button";
+import Link from "next/link";
+
+interface BadgeProps {
+  index: number;
+  image: string;
+  title: string;
+  unlocked?: boolean;
+}
+
+export default function Badge({ image, title, index, unlocked = true }: BadgeProps) {
+  return (
+    <motion.div
+      className="flex flex-col justify-start items-center w-full p-3 gap-2 bg-secondary rounded-lg"
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: unlocked ? 1 : 0.5, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.15 }}
+    >
+      <h1 className="text-sm text-nowrap font-black">{title}</h1>
+      <img alt="badge image" src={image}></img>
+      <Button className="w-full h-6">
+        <Link href={`/user/badges/${index}`}>Details</Link>
+      </Button>
+    </motion.div>
+  );
+}
