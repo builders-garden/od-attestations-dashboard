f08283b3766e4077265a9e72e04feb0fa6ddc114
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-11-06 17:37:49 +0100

    Created more constants to test

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,28 @@
+import BadgeClass from "./classes/BadgeClass";
+
 // Add your admin address here to test
 export const ADMIN_ADDRESSES = ["0x82A29547CA8970c2aDECF4C2db7e364339f9a4B7"];
+
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+// Badges used to test the UI
+export const badges = [
+  new BadgeClass(1, "/badges/badge1.png", "First Buyer", true, lorem),
+  new BadgeClass(2, "/badges/badge2.png", "Onchain Sherpa", true, lorem),
+  new BadgeClass(3, "/badges/badge3.png", "MEV Slayer", true, lorem),
+  new BadgeClass(4, "/badges/badge4.png", "Spirit Guide", false, lorem),
+  new BadgeClass(5, "/badges/badge5.png", "Pulcino Pio", false, lorem),
+  new BadgeClass(6, "/badges/badge6.png", "OCCHIO BRUCIA AHIA!!", false, lorem),
+];
+
+// Users used to test the UI
+export const collectors = [
+  "limone.eth",
+  "0x82A29547........364339f9a4B7",
+  "0x82A29547........364339f9a4B7",
+  "0x82A29547........364339f9a4B7",
+  "blackicon.eth",
+  "0x82A29547........364339f9a4B7",
+  "camparimaximalist.eth",
+];
