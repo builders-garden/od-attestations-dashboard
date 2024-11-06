e61df3bb42a27e6f82f39a6c57322394204ef357
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-11-06 17:37:56 +0100

    Created badge class

---
+++ lib/classes/BadgeClass.ts
@@ -0,0 +1,27 @@
+class Badge {
+  index: number;
+  image: string;
+  title: string;
+  description: string;
+  unlocked: boolean;
+
+  constructor(index: number, image: string, title: string, unlocked: boolean = true, description: string) {
+    this.index = index;
+    this.image = image;
+    this.title = title;
+    this.unlocked = unlocked;
+    this.description = description;
+  }
+
+  getBadgeInfo() {
+    return {
+      index: this.index,
+      image: this.image,
+      title: this.title,
+      description: this.description,
+      unlocked: this.unlocked,
+    };
+  }
+}
+
+export default Badge;
