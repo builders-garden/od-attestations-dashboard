eec3eac30919815628a7daf2ed401cb1d0293f45
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-11-25 11:37:54 +0100

    changed index to uid

--- app/user/badge/[uid]/reissue/page.tsx
+++ app/user/badge/[uid]/reissue/page.tsx
@@ -14,35 +14,36 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { LinkTextWithIcon } from "@/components/ui/linkTextWithIcon";
-import { collectors, userBadges } from "@/lib/constants";
+import { userBadges } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { use, useState } from "react";
 
-export default function BadgeRevokePage({
+export default function BadgeReissuePage({
   params,
 }: {
-  params: Promise<{ index: string }>;
+  params: Promise<{ uid: string }>;
 }) {
-  const { index } = use(params);
+  const { uid } = use(params);
 
-  const badge = userBadges[parseInt(index) - 1];
-
-  const [selectedCollectors, setSelectedCollectors] = useState<string[]>([]);
-
-  const handleSelect = (collector: string) => {
-    setSelectedCollectors((prev) =>
-      prev.includes(collector)
-        ? prev.filter((c) => c !== collector)
-        : [...prev, collector],
-    );
-  };
+  const badge = userBadges[parseInt(uid) - 1];
 
   const [input, setInput] = useState("");
 
-  const atLeastOneSelected = selectedCollectors.length > 0;
+  const [collectors, setCollectors] = useState<string[]>([]);
+
+  const handleRemove = (collector: string) => {
+    setCollectors((prev) => prev.filter((c) => c !== collector));
+  };
+
+  const handleAdd = (collector: string) => {
+    setCollectors((prev) =>
+      prev.includes(collector) ? prev : [...prev, collector],
+    );
+    setInput("");
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen w-full bg-background">
@@ -59,81 +60,78 @@ export default function BadgeRevokePage({
             transition={{ duration: 0.5 }}
             className="flex justify-between items-center w-full"
           >
-            <Link href={`/user/badge/${index}`} className="rounded-full">
+            <Link href={`/user/badge/${uid}`} className="rounded-full">
               <ArrowLeft size={24} />
             </Link>
-            <h1 className="font-black text-2xl">Revoke 🚫</h1>
+            <h1 className="font-black text-2xl">Reissue 📤</h1>
           </motion.div>
 
           <span className="w-full">
-            Select one or more users to revoke the selected badge from them.
+            Insert one or more users to reissue the selected badge to them.
           </span>
 
           <div className="grid grid-cols-1 justify-start items-center gap-3 w-full">
             <div className="flex w-full justify-between">
-              <span className="font-bold">{badge.title} collectors</span>
+              <span className="font-bold">New {badge.title} collectors</span>
               <LinkTextWithIcon>Easscan</LinkTextWithIcon>
             </div>
             <div
               className={cn(
                 "flex w-full gap-0 justify-between",
-                atLeastOneSelected && "gap-4",
+                input.length > 0 && "gap-4",
               )}
             >
               <Input
-                placeholder="Search..."
-                className="focus-visible:ring-primary w-full"
+                placeholder="ENS or Address..."
+                className="w-full"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
               />
               <Button
-                onClick={() => setSelectedCollectors([])}
+                onClick={() => handleAdd(input)}
                 className={cn(
                   "w-fit transition-all duration-200 ease-in-out",
-                  atLeastOneSelected && "w-fit px-4",
-                  !atLeastOneSelected && "w-0 p-0",
+                  input.length > 0 && "w-fit px-4",
+                  input.length === 0 && "w-0 p-0",
                 )}
               >
-                Reset Selection
+                Add
               </Button>
             </div>
             <div className="flex flex-col gap-3 w-full max-h-[50rem] overflow-y-auto">
-              {collectors
-                .filter((collector) => collector.includes(input))
-                .map((collector, index) => (
-                  <CollectorRow
-                    key={index}
-                    collector={collector}
-                    selectable
-                    selected={selectedCollectors.includes(collector)}
-                    onClick={() => handleSelect(collector)}
-                  />
-                ))}
+              {collectors.map((collector, index) => (
+                <CollectorRow
+                  key={index}
+                  collector={collector}
+                  removable
+                  handleRemove={handleRemove}
+                />
+              ))}
             </div>
           </div>
         </div>
         <Dialog>
           <DialogTrigger asChild>
             <Button
-              variant="destructive"
               className={cn(
                 "text-2xl px-8 py-6 rounded-lg w-full transition-opacity duration-200 ease-in-out",
-                atLeastOneSelected && "opacity-1",
-                !atLeastOneSelected && "opacity-0",
+                collectors.length > 0 && "opacity-1",
+                collectors.length === 0 && "opacity-0",
               )}
+              variant="green"
             >
-              Revoke
+              Reissue
             </Button>
           </DialogTrigger>
           <DialogContent className="max-w-sm gap-6">
             <DialogHeader>
               <DialogTitle className="text-center text-2xl font-extrabold">
-                Confirm Revocation
+                Confirm Reissuance
               </DialogTitle>
             </DialogHeader>
             <DialogDescription className="text-center">
-              Are you sure you want to permanently revoke this badge from the
-              selected users? This action cannot be undone.
+              Are you sure you want to reissue this badge to the users you
+              selected?
             </DialogDescription>
             <DialogFooter className="sm:justify-start">
               <DialogClose asChild>
@@ -141,8 +139,8 @@ export default function BadgeRevokePage({
                   Cancel
                 </Button>
               </DialogClose>
-              <Button type="button" variant="destructive" className="w-full">
-                Revoke
+              <Button type="button" variant="green" className="w-full">
+                Reissue
               </Button>
             </DialogFooter>
           </DialogContent>
