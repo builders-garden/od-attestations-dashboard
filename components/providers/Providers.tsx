08a2475b9bb5a42df202e342ddb27ce53af299c4
caso <matteocasonato@protonmail.com>
2024-12-02 17:21:15 +0100

    Revert "test safe"
    This reverts commit 6d32ac23b172b0d110b5ac588ecd2003fcf0472c.

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -1,34 +1,14 @@
 "use client";
 import "@rainbow-me/rainbowkit/styles.css";
-import {
-  connectorsForWallets,
-  getDefaultConfig,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-import { createConfig, http, WagmiProvider } from "wagmi";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { http, WagmiProvider } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactNode } from "react";
-import {
-  safeWallet,
-  walletConnectWallet,
-} from "@rainbow-me/rainbowkit/wallets";
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: "Recommended",
-      wallets: [walletConnectWallet, safeWallet],
-    },
-  ],
-  {
-    appName: "OD Passport App",
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  },
-);
-
-const config = createConfig({
-  connectors,
+const config = getDefaultConfig({
+  appName: "OD Passport App",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
   chains: [sepolia],
   transports: {
     [sepolia.id]: http(
@@ -37,18 +17,6 @@ const config = createConfig({
   },
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
-
-// const config = getDefaultConfig({
-//   appName: "OD Passport App",
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-//   chains: [sepolia],
-//   transports: {
-//     [sepolia.id]: http(
-//       "https://eth-sepolia.g.alchemy.com/v2/cZS5C6pUs9vGcs7yWAo6Pv9Q28vfhBcI", // TODO: change it to OD Alchemy key from .env
-//     ),
-//   },
-//   ssr: true, // If your dApp uses server side rendering (SSR)
-// });
 
 const queryClient = new QueryClient();
 
