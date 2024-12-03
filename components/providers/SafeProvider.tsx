0dfbf5f0d3b24cb7d0fbff78e457c8a8c4a51294
lim0n3 <simonestaffa96@gmail.com>
2024-12-03 17:10:38 +0800

    fix safe provider context

--- components/providers/SafeProvider.tsx
+++ components/providers/SafeProvider.tsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 import SafeFactory from "@safe-global/protocol-kit";
 import SafeApiKit from "@safe-global/api-kit";
-import { Config, useConnectorClient, useChainId } from "wagmi";
+import { Config, useConnectorClient, useChainId, useAccount } from "wagmi";
 import { useMemo } from "react";
 import type { Account, Chain, Client, Transport } from "viem";
 import { ethers, providers } from "ethers";
@@ -25,28 +25,11 @@ export function clientToSigner(client: Client<Transport, Chain, Account>) {
   return signer;
 }
 
-export function clientToProvider(client: Client<Transport, Chain, Account>) {
-  const { account, chain, transport } = client;
-  const network = {
-    chainId: chain.id,
-    name: chain.name,
-    ensAddress: chain.contracts?.ensRegistry?.address,
-  };
-  const provider = new providers.Web3Provider(transport, network);
-  return provider;
-}
-
 /** Hook to convert a Viem Client to an ethers.js Signer. */
 export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
   const { data: client } = useConnectorClient<Config>({ chainId });
   // @ts-ignore
   return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
-}
-
-export function useEthersProvider({ chainId }: { chainId?: number } = {}) {
-  const { data: client } = useConnectorClient<Config>({ chainId });
-  // @ts-ignore
-  return useMemo(() => (client ? clientToProvider(client) : undefined), [client]);
 }
 
 const ProtocolKitOwnerContext = createContext<SafeFactory | null>(null);
@@ -60,24 +43,25 @@ export const SafeProvider: React.FC<{ children: ReactNode }> = ({
 }) => {
   const chainId = useChainId();
 
-  const signer = useEthersSigner({ chainId });
-  const provider = useEthersProvider({ chainId });
+  const { address } = useAccount();
 
   const [protocolKitOwner, setProtocolKitOwner] = useState<SafeFactory | null>(
     null,
   );
-
   const [apiKit, setApiKit] = useState<SafeApiKit | null>(null);
 
   const init = async () => {
+    if (!address) {
+      console.error("Impossible to init SafeProvider without an address");
+      return;
+    }
     const protocolKitOwner = await SafeFactory.init({
       provider: window.ethereum,
-      signer: await signer?.getAddress(),
+      signer: address,
       safeAddress: "0x883ac919B42b9065C1Bc1Ea7560ba2924655762E",
     });
     const apiKit = new SafeApiKit({
       chainId: BigInt(sepolia.id), // set the correct chainId
-      // txServiceUrl: "https://safe-transaction-sepolia.safe.global",
     });
     setProtocolKitOwner(protocolKitOwner);
     setApiKit(apiKit);
@@ -85,7 +69,7 @@ export const SafeProvider: React.FC<{ children: ReactNode }> = ({
 
   useEffect(() => {
     init();
-  }, [signer]);
+  }, [address]);
 
   return (
     <ProtocolKitOwnerContext.Provider value={protocolKitOwner}>
